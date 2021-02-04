c9cff663d4a2b11444b910c5b48545871cb5001e
Teg Dhillon <teginderdhillon@gmail.com>
2021-02-03 23:57:13 -0500

    first commit

---
+++ TeginderpalD_301093892/TeginderpalD_301093892/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeginderpalD_301093892.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult AddFaculty()
+        {
+            return View();
+        }
+
+        public ViewResult Faculty()
+        {
+            return View();
+        }
+
+        public ViewResult AddCourse()
+        {
+            return View();
+        }
+
+        public ViewResult Course()
+        {
+            return View();
+        }
+    }
+}
